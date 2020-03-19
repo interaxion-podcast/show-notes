4da16741679120686a627437ac3df517eaf276db
oka <oka@stu.kobe-u.ac.jp>
2020-03-19 21:47:26 +0900

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 for md in $(ls md/*.md); do
-    num=$(echo $md | /usr/bin/sed 's/.*ep\(.*\).md/\1/g')
+    num=$(echo $md | sed 's/.*ep\(.*\).md/\1/g')
     pandoc -f markdown -t html md/ep$num.md -o html/ep$num.html
 done
 

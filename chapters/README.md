c83529436d2a8a67b3b9c9d45aef01d1184da738
oka <oka@stu.kobe-u.ac.jp>
2020-05-18 17:49:45 +0900

    add action for creating chapter

---
+++ chapters/README.md
@@ -0,0 +1,1 @@
+Forked from [rui314/add-chapters.py](https://gist.github.com/rui314/6e435fcebe3998333d37904e893c8c12).
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,19 @@ for md in $(ls md/*.md); do
     pandoc -f markdown-auto_identifiers -t html md/ep$num.md -o html/ep$num.html
 done
 
+## Create YAML for adding chapters in MP3.
+
+yml="chapters/episodes.yml"
+
+echo "---" > $yml
+
+for md in $(ls -1 md/*.md)
+do
+    epnum=$(echo $md | gsed --regex 's/.*ep([0-9]+)\.md/\1/')
+    echo "episode: $epnum" >> $yml
+    title=$(cat $md | gsed -n --regex 's/.*[Ee]p\.[\s ][0-9+][\s ](.*)/\1/p')
+    echo "title: $title" >> $yml
+    echo "topics:" >> $yml
+    cat $md | gsed -n --regex 's/(#+)\s([0-9:]+)\s(.+)/\3(\2\)/gp' >> $yml
+    echo "---" >> $yml
+done

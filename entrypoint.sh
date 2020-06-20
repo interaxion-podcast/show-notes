892305302a961ab0912461b4ad86d54236b341e8
oka <pn11@users.noreply.github.com>
2020-06-21 01:54:23 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 # URL 短縮
 for md in $(ls md/*.md); do
-    echo $FDL_TOKEN
-    python3 scripts/shorten_urls_md.py $md i8n $FDL_TOKEN > tmp.md
+    python3 scripts/shorten_urls_md.py $md i8n $FDL_KEY > tmp.md
     mv tmp.md $md
 done
 

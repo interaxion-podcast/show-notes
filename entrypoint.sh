3f2110f8733dd4477f06fbeb89ba691b596ba5ab
oka <pn11@users.noreply.github.com>
2020-09-24 22:58:13 +0900

    fix title extracting

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,9 +22,9 @@ echo "---" > $yml
 
 for md in $(ls -1 md/*.md)
 do
-    epnum=$(echo $md | sed --regex 's/.*ep([0-9]+)\.md/\1/')
+    epnum=$(echo $md | head -1 | sed --regex 's/.*ep([0-9]+)\.md/\1/')
     echo "episode: $epnum" >> $yml
-    title=$(cat $md | sed -n --regex 's/.*[Ee]p\.[\s ][0-9+][\s ](.*)/\1/p')
+    title=$(cat $md | head -1 |sed -n --regex 's/.*[Ee]p\.[\s ][0-9+][\s ](.*)/\1/p')
     echo "title: $title" >> $yml
     echo "topics:" >> $yml
     cat $md | sed -n --regex 's/(#+)\s([0-9:]+)\s(.+)/\3(\2\)/gp' | awk '{printf "    - %s\n", $0}'>> $yml
